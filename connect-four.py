18af06021fd8d95c8fcdc7de6238985cebf77252
kaizen95coder <kaizen95coder@gmail.com>
2023-02-10 18:56:28 +0000

    added html and css files

--- connect-four.py
+++ connect-four.py
@@ -3,15 +3,17 @@
 A traditional Connect Four board is 7 columns and 6 rows, but feel free to make your board as big as you like! """
 
 # Create the board
+
+
 def print_board():
     board = [['.' for i in range(7)] for j in range(6)]
     print('1 2 3 4 5 6 7')
     for row in range(6):
         print(' '.join(board[row]))
 
-print_board()
-
 # Add a piece to the board
+
+
 def add_piece(board, col, piece):
     for row in range(5, -1, -1):
         if board[row][col] == '.':
@@ -25,28 +27,52 @@ def add_piece(board, col, piece):
         else:
             print('Something went wrong!')
 
-def is_game_over():
-    # Check for a winner
-    pass
-    # Check for a tie
-    pass
+# Check if the game is over
+
+
+def is_game_over(board):
+    for row in range(6):
+        for col in range(7):
+            if board[row][col] == '.':
+                continue
+            if col <= 3 and board[row][col] == board[row][col + 1] == board[row][col + 2] == board[row][col + 3]:
+                return True
+            if row <= 2 and board[row][col] == board[row + 1][col] == board[row + 2][col] == board[row + 3][col]:
+                return True
+            if row <= 2 and col <= 3 and board[row][col] == board[row + 1][col + 1] == board[row + 2][col + 2] == board[row + 3][col + 3]:
+                return True
+            if row <= 2 and col >= 3 and board[row][col] == board[row + 1][col - 1] == board[row + 2][col - 2] == board[row + 3][col - 3]:
+                return True
+    for row in range(6):
+        for col in range(7):
+            if board[row][col] == '.':
+                return False
+    return True
+
+# Play the game
+
 
 def connect_four_game():
     board = [['.' for i in range(7)] for j in range(6)]
     print('1 2 3 4 5 6 7')
     for row in range(6):
         print(' '.join(board[row]))
-    while not is_game_over():
-        input('Player 1, enter a column: ')
-        add_piece(board, col, 'X')
+    player = 1
+    while not is_game_over(board):
+        if player == 1:
+            piece = 'X'
+        else:
+            piece = 'O'
+        col = int(input(f'Player {player}, enter a column (1-7): ')) - 1
+        add_piece(board, col, piece)
         print_board()
-        if is_game_over():
+        if is_game_over(board):
             break
-        input('Player 2, enter a column: ')
-        add_piece(board, col, 'O')
-        print_board()
-        if is_game_over():
-            break
+        player = 3 - player
     print_board()
-    # Print the winner
-    pass
+    if is_game_over(board) == 'tie':
+        print("It's a tie!")
+    else:
+        print(f"Player {3 - player} wins!")
+
+connect_four_game()
