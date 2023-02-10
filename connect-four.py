81eedbed052da586e25dba8cead0157f4f8f4d0f
kaizen95coder <kaizen95coder@gmail.com>
2023-02-10 18:32:40 +0000

    added add_piece function

--- connect-four.py
+++ connect-four.py
@@ -4,10 +4,27 @@ A traditional Connect Four board is 7 columns and 6 rows, but feel free to make 
 
 # Create the board
 def print_board():
-    board = [['|' for i in range(8)] for j in range(6)]
-    print(' 1 2 3 4 5 6 7')
-    print(' ' * 7)
+    board = [['.' for i in range(7)] for j in range(6)]
+    print('1 2 3 4 5 6 7')
     for row in range(6):
-        print(' '.join(board[row]) + '\n' + ' -' * 7)
+        print(' '.join(board[row]))
 
 print_board()
+
+# Add a piece to the board
+def add_piece(board, col, piece):
+    for row in range(5, -1, -1):
+        if board[row][col] == '.':
+            board[row][col] = piece
+            break
+        elif row == 0:
+            print('That column is full!')
+        elif board[row][col] != '.':
+            continue
+        elif board[row][col] == piece:
+            print('That piece is already there!')
+            break
+        else:
+            print('Something went wrong!')
+
+
